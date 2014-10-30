c9aab4037440e77e27969c7425162debf6ea5fa6
Larzo <wbsurfver@gmail.com>
2014-10-30 16:37:58 -0400

    optimize out 'Blue Distinction Plus'

--- award.rb
+++ award.rb
@@ -18,9 +18,11 @@ class Award
     awards.each{|award| award.update_quality}
   end
 
-  def update_quality
-    upd_quality
-    upd_expiration
+   def update_quality
+    if self.name != 'Blue Distinction Plus'
+      upd_quality
+      upd_expiration
+    end
   end
 
   private
@@ -28,25 +30,23 @@ class Award
   # update quality field based on current value and award name
   def upd_quality
     award = self
-    if award.name != 'Blue Distinction Plus'
-      if ['Blue First', 'Blue Compare'].include?(award.name)
-        if award.quality < 50
-          award.quality += 1
-          if award.name == 'Blue Compare'
-            if award.expires_in < 6
-              award.quality += 2 # by 3
-            elsif award.expires_in < 11
-              award.quality += 1	# by 2
-            end
+    if ['Blue First', 'Blue Compare'].include?(award.name)
+      if award.quality < 50
+        award.quality += 1
+        if award.name == 'Blue Compare'
+          if award.expires_in < 6
+            award.quality += 2 # by 3
+          elsif award.expires_in < 11
+            award.quality += 1	# by 2
           end
-        award_quality = 50 if award.quality > 50
         end
-      elsif award.quality > 0
-        if award.name == 'Blue Star'
-          award.quality -= 2
-        else
-          award.quality -= 1
-        end
+      award_quality = 50 if award.quality > 50
+      end
+    elsif award.quality > 0
+      if award.name == 'Blue Star'
+        award.quality -= 2
+      else
+        award.quality -= 1
       end
     end
   end
@@ -58,26 +58,23 @@ class Award
   #
   def upd_expiration
     award = self
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-
-      if award.expires_in < 0
-        if award.name != 'Blue First'
-          if award.name != 'Blue Compare'
-            if award.quality > 0
-              if award.name == 'Blue Star'
-                award.quality -= 2
-              else
-                award.quality -= 1
-              end
+    award.expires_in -= 1
+    if award.expires_in < 0
+      if award.name != 'Blue First'
+        if award.name != 'Blue Compare'
+          if award.quality > 0
+            if award.name == 'Blue Star'
+              award.quality -= 2
+            else
+              award.quality -= 1
             end
-          else
-            award.quality = 0
           end
-        elsif award.quality < 50
-        # blue first scenario
-          award.quality += 1
+        else
+          award.quality = 0
         end
+      elsif award.quality < 50
+      # blue first scenario
+        award.quality += 1
       end
     end
   end
